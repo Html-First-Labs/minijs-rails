1f1fb84ba84877841345d7ba257cf9c20fc83444
Tony Ennis <ennis.tony@gmail.com>
2023-12-16 22:10:05 +0100

    Add tomselect js

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,8 +2,18 @@ function initializeTippy() {
   tippy('[data-tippy-content]');
 }
 
+function initializeTomSelect() {
+  document.querySelectorAll('.custom-select').forEach((el)=>{
+    let settings = {
+      highlight: el.getAttribute('data-highlight')
+    };
+    new TomSelect(el,settings);
+  });
+}
+
 function reinitializeJs() { 
   initializeTippy();
+  initializeTomSelect();
 }
 
 document.addEventListener("DOMContentLoaded", function(event){
